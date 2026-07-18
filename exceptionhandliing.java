88040faf833ee5cdf7e853d30d8e25eadd0bf68e
Deep <deep@Deeps-MacBook-Air.local>
2026-07-18 11:37:16 +0530

    Auto-commit: ._exceptionhandliing.class, exceptionhandliing.class, exceptionhandliing.java at 18/07/2026, 11:37:16

---
+++ exceptionhandliing.java
@@ -0,0 +1,15 @@
+public class exceptionhandliing {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 20;
+        try{
+            if(a+b != 50){
+                throw new Exception("the input value is wrong");
+            }
+            System.out.println("the sum is: 50");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
