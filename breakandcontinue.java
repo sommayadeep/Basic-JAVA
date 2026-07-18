716d0ded0e6f49cdb78c12ee795a6e89d9802289
Deep <deep@Deeps-MacBook-Air.local>
2026-07-18 11:04:06 +0530

    Auto-commit: ._breakandcontinue.class, ._breakandcontinue.java, boolian.java, breakandcontinue.class, breakandcontinue.java at 18/07/2026, 11:04:06

---
+++ breakandcontinue.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class breakandcontinue {
+    public static void main(String[] args){
+        System.out.print("enter: ");
+        Scanner sc = new Scanner(System.in)  ;
+        int numbers = sc.nextInt();
+        System.out.println(numbers);
+    }
+}
