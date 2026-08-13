bdedbeca6896b60594dbb82bca82949299ebf43c
Deep <deep@Deeps-MacBook-Air.local>
2026-08-13 09:34:24 +0530

    chore: update staticfuntion.java and add compiled class files

--- staticfuntion.java
+++ staticfuntion.java
@@ -4,9 +4,9 @@ public class staticfuntion {
         int b = 20;
         int c = 30;
 
-        System.out.prinln("a = " + a);
+        System.out.println("a = " + a);
         System.out.println("b =" + b);
-        System.out.prinln("c = " + c);
+        System.out.println("c = " + c);
         }
 
         public static void main(String[] args){
