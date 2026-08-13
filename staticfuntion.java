cec2db2a60684f193f62cb77e03e4b5e5258a0cf
Deep <deep@Deeps-MacBook-Air.local>
2026-08-13 09:34:20 +0530

    feat: add new functionality to staticfuntion.java

---
+++ staticfuntion.java
@@ -0,0 +1,15 @@
+public class staticfuntion {
+    static void display(){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        System.out.prinln("a = " + a);
+        System.out.println("b =" + b);
+        System.out.prinln("c = " + c);
+        }
+
+        public static void main(String[] args){
+            display();
+        }
+}
