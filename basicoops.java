312708825100595164442ef95156133cf30321b1
Deep <deep@Deeps-MacBook-Air.local>
2026-08-27 10:21:44 +0530

    feat: add new Java classes and update existing ones in project

---
+++ basicoops.java
@@ -0,0 +1,35 @@
+class Pen{
+    String colour;
+    String type;
+
+    public void write(){
+        System.out.println("Output Something");
+    }
+
+    public void printcolour(){
+        System.out.print(this.colour );  //this. is used for calling the colour part
+        System.out.println(this.type); 
+    }
+}
+
+
+public class basicoops {
+    public static void main(String args[]){
+        Pen pen1 = new Pen();
+        pen1.colour = "blue";
+        pen1.type = "ball pen";
+
+        pen1.write();  // It print only "Output something"
+
+        pen1.printcolour();  // call the function of printcolour to print the pen1
+
+
+
+        Pen pen2 = new Pen();
+        pen2.colour = "black";
+        pen2.type = "gel";
+
+        pen2.printcolour(); // call the function of printcolour to print the pen2
+    }
+    
+}
---
+++ perameterised.java
@@ -0,0 +1,23 @@
+class Student{
+    String name;
+    int age;
+    
+    Student( String name, int age){
+        this.name = name;
+        this.age = age;
+
+    }
+    public void print(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+
+}
+public class perameterised {
+    public static void main(String[] args){
+        Student s1 = new Student("Deep" , 20);  //---------------perameterised------------------
+    
+        s1.print();
+
+    }
+}
